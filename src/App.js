125c844bdd3253233789f38a105e54a504b9e1b5
Syed Muhammad Mujtaba Naqvi <smmujtabanaqvi315@gmail.com>
2024-07-23 00:27:43 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import Login from './components/Login';
 import './App.css';
+
+var isLoggedIn = false;
+
+function renderConditionally () {
+  if (isLoggedIn === true) {
+    return <h1>Hello</h1> ;
+  } else {
+    return (
+      <Login />
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      {renderConditionally()}
     </div>
   );
 }
